e2dffa22b34722d73466477237c9ff0205d5cd3a
HallBM <bhall.phd@gmail.com>
2023-01-26 19:46:08 -0500

    Create RegexTesting.java

---
+++ Core Java/src/testing/RegexTesting.java
@@ -0,0 +1,13 @@
+package testing;
+
+public class RegexTesting {
+
+	public static void main(String[] args) {
+		String choice = "12349";
+		
+		System.out.print(choice.matches("\\d+"));
+		
+
+	}
+
+}
